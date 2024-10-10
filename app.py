f71b3bad08c4a78e1114bf78da12020099e6078e
ZB Siu <siuzhuobin@gmail.com>
2024-10-11 06:58:02 +0800

    updated a bit

--- app.py
+++ app.py
@@ -4,19 +4,39 @@ import os
 import markdown
 import textblob
 import json
+import re
 
 
-api_key="AIzaSyDQvy9nXbikgMd890MAyt-penTiMoM7Vek"  # aiyah by right supposed to be environmetal var; by left whatever lah 
-
-model = genai.GenerativeModel('gemini-1.5-flash')
-genai.configure(api_key="AIzaSyDQvy9nXbikgMd890MAyt-penTiMoM7Vek")
 
 app = Flask(__name__) # erm apparently this is some signing thingy, e.g. for  apps to access like video camera or whatever 
 
+# get static folder 
+static_folder_path = os.path.join(app.root_path, 'static')
+food_data_file = os.path.join(static_folder_path,'food_data.json')
 
-@app.route("/", methods=["GET","POST"])  # that @ is a deocrator - which makes the app.route run before the index function 
+with open(food_data_file, 'r') as file:
+    food_data = json.load(file)
+
+
+food_items_str='['
+food_items_list = list(food_data.keys())
+for f in range(len(food_items_list)-1):
+    food_items_str=food_items_str + '"' + food_items_list[f].title() + '",'
+food_items_str = food_items_str + '"' + food_items_list[-1].title() + '"]'
+print("ff")
+print(food_items_str)
+
+
+@app.route("/", methods=["GET","POST"])  # that @ is a deocorator - which makes the app.route run before the index function 
 def index():
     return(render_template("index.html"))
+    
+    
+    
+@app.route("/food_info", methods=["GET","POST"]) 
+def food_info():
+    return(render_template("food_info.html", f = food_items_str))
+
 
 
 @app.route("/generate_shopping_list", methods=["GET","POST"])  # that @ is a deocrator - which makes the app.route run before the index function 
@@ -46,9 +66,14 @@ def generate_shopping_list():
         allergy_str = allergy_str + user_allergies[-1] + '.'
     request_str = request_str + allergy_str
     result_str = request_str
-    result_str = markdown.markdown(model.generate_content(request_str).text)
+    raw_html = markdown.markdown(model.generate_content(request_str).text)
     
-    return(render_template("shopping_list.html", r = result_str))
+    proc_html = raw_html
+    for k in food_data.keys():
+        rep_str = '<a href=# onclick=get_food_date("' + k +'")' + '>' + k.title() + '</a>'
+        proc_html = re.sub(k, rep_str, proc_html, flags=re.IGNORECASE)
+    return(render_template("shopping_list.html", r = proc_html))
+
 
 @app.route("/subscription",methods=["GET","POST"])
 def subscription():
@@ -59,7 +84,10 @@ def thankyou():
     r = model.generate_content() 
     return render_template("thankyou.html", r=r)
 
+
+
+model = genai.GenerativeModel('gemini-1.5-flash')
+genai.configure(api_key="AIzaSyDQvy9nXbikgMd890MAyt-penTiMoM7Vek")
 if __name__ == "__main__": # apparently, this checks if the thingy is being run onto the cloud? 
     app.run()
-
 
